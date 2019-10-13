462cf0496866d34a9503801b78820d8e8da45f25
Jeremy Wolcott <jeremy.wolcott@tufts.edu>
2019-10-13 15:34:49 -0500

    don't support literally all the calcs we don't need

--- CAFAna/Core/LoadFromFile.cxx
+++ CAFAna/Core/LoadFromFile.cxx
@@ -1,12 +1,7 @@
 #include "CAFAna/Core/LoadFromFile.h"
 
 #include "OscLib/func/OscCalculator.h"
-#include "OscLib/func/OscCalculatorDumb.h"
-#include "OscLib/func/OscCalculatorGeneral.h"
-#include "OscLib/func/OscCalculatorPMNS.h"
 #include "OscLib/func/OscCalculatorPMNSOpt.h"
-#include "OscLib/func/OscCalculatorSterile.h"
-#include "OscLib/func/OscCalculatorPMNS_NSI.h"
 
 #include "TObjString.h"
 #include "TH1.h"
@@ -24,16 +19,11 @@ namespace ana
     const TString tag = ptag->GetString();
 
     if(tag == "NoOscillations") return std::unique_ptr<osc::IOscCalculator>(new osc::NoOscillations);
-    if(tag == "OscCalculatorDumb") return std::unique_ptr<osc::IOscCalculator>(new osc::OscCalculatorDumb);
 
     osc::IOscCalculatorAdjustable* ret = 0;
 
     if(tag == "OscCalculator") ret = new osc::OscCalculator;
-    if(tag == "OscCalculatorGeneral") ret = new osc::OscCalculatorGeneral;
-    if(tag == "OscCalculatorPMNS") ret = new osc::OscCalculatorPMNS;
     if(tag == "OscCalculatorPMNSOpt") ret = new osc::OscCalculatorPMNSOpt;
-    if(tag == "OscCalculatorSterile") ret = new osc::OscCalculatorSterile;
-    if(tag == "OscCalculatorPMNS_NSI") ret = new osc::OscCalculatorPMNS_NSI;
 
     if(!ret){
       std::cout << "LoadFrom not implemented for " << tag << std::endl;
@@ -42,25 +32,6 @@ namespace ana
 
     TVectorD* params = (TVectorD*)dir->Get("params");
     assert(params);
-    //special case how OscCalculatorSterile is initialized
-    if(tag == "OscCalculatorSterile") {
-      std::vector<double> state;
-      for (int i = 0; i < params->GetNrows(); ++i) {
-	state.push_back( (*params)[i] );
-      }
-      dynamic_cast<osc::OscCalculatorSterile*>(ret)->SetState(state);
-      return std::unique_ptr<osc::IOscCalculatorAdjustable>(ret);
-    }
-    //special case how OscCalculatorPMNS_NSI is initialized
-    if(tag == "OscCalculatorPMNS_NSI") {
-      std::vector<double> state;
-      for (int i = 0; i < params->GetNrows(); ++i) {
-	state.push_back( (*params)[i] );
-      }
-      dynamic_cast<osc::OscCalculatorPMNS_NSI*>(ret)->SetState(state);
-      return std::unique_ptr<osc::IOscCalculatorAdjustable>(ret);
-    }
-
     assert(params->GetNrows() == 8);
 
     ret->SetL     ((*params)[0]);
@@ -88,44 +59,13 @@ namespace ana
       return;
     }
 
-    if(dynamic_cast<const osc::OscCalculatorDumb*>(&x)){
-      TObjString("OscCalculatorDumb").Write("type");
-      tmp->cd();
-      return;
-    }
-
     const osc::IOscCalculatorAdjustable* y = dynamic_cast<const osc::IOscCalculatorAdjustable*>(&x);
     if(!y){
       std::cout << "Unknown calculator in SaveTo " << typeid(x).name() << std::endl;
       abort();
     }
     
-    const osc::OscCalculatorSterile* tmpSterile = dynamic_cast<const osc::OscCalculatorSterile*>(&x);
-    if(tmpSterile) {
-      TObjString("OscCalculatorSterile").Write("type");      
-      std::vector<double> state = tmpSterile->GetState();
-      TVectorD params(state.size());
-      for (unsigned int i = 0; i < state.size(); ++i) params[i] = state[i];
-      params.Write("params");
-      tmp->cd();
-      return;
-    }
-
-    //for the implementation of OscCalculatorPMNS_NSI
-    const osc::OscCalculatorPMNS_NSI* tmpNSI = dynamic_cast<const osc::OscCalculatorPMNS_NSI*>(&x);
-    if(tmpNSI) {
-      TObjString("OscCalculatorPMNS_NSI").Write("type");      
-      std::vector<double> state = tmpNSI->GetState();
-      TVectorD params(state.size());
-      for (unsigned int i = 0; i < state.size(); ++i) params[i] = state[i];
-      params.Write("params");
-      tmp->cd();
-      return;
-    }
-
     /* */if(dynamic_cast<const osc::OscCalculator*>(&x)) TObjString("OscCalculatorPMNS").Write("type");
-    else if(dynamic_cast<const osc::OscCalculatorGeneral*>(&x)) TObjString("OscCalculatorGeneral").Write("type");
-    else if(dynamic_cast<const osc::OscCalculatorPMNS*>(&x)) TObjString("OscCalculatorPMNS").Write("type");
     else if(dynamic_cast<const osc::OscCalculatorPMNSOpt*>(&x)) TObjString("OscCalculatorPMNSOpt").Write("type");
     else{
       std::cout << "Unimplemented calculator in SaveTo " << typeid(x).name() << std::endl;
